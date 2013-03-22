7750b900b6ab6f136aee64fb806c95b0bfef1d8d
Davide Rizzo <davide@metwit.com>
2013-03-22 17:28:16 +0100

    set Metwit base url to api.metwit.com

--- metwit/metwit.py
+++ metwit/metwit.py
@@ -2,11 +2,9 @@ from .rest import Resource, RestApi
 
 
 class Metwit(RestApi):
-    #base_url = "https://api.metwit.com/"
-    #token_url = 'https://api.metwit.com/token/'
+    base_url = "https://api.metwit.com/"
+    token_url = 'https://api.metwit.com/token/'
     dialog_url = "https://metwit.com/oauth/authorize/"
-    base_url = "http://127.0.0.1:8000/"
-    token_url = 'http://127.0.0.1:8000/token/'
 
     weather = Resource('/v2/weather/')
     metags = Resource('/v2/metags/')
